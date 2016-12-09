069d72b89453b12cc2f8378a031f8456c64a2486
Flandresxl <flandresxl@outlook.com>
2016-12-09 23:56:51 +0800

    。

--- src/ml/lsxorz/HelloWorld.java
+++ src/ml/lsxorz/HelloWorld.java
@@ -3,5 +3,7 @@ package ml.lsxorz;
 public class HelloWorld {
 	public static void main(String[] args){
 		System.out.println("Hello, World!");
+		System.out.println("Hello!");
+		System.out.println("Hello!");
 	}
 }
